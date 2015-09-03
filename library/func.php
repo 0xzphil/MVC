cb8b2e6aaf6c8e2fb99cb9b1ae58a8bde7a8002b
Piddos <df.gomu@gmail.com>
2015-09-03 15:57:15 +0700

    Update ver 1.3.4

--- library/func.php
+++ library/func.php
@@ -1,31 +1,21 @@
 <?php
-namespace library;
-/**
-* 
-*/
-use models\Base_Model;
-class Func
-{
-	public $conn;
+	namespace library;
+	class Func
+	{
+		function get_time_zone($gmt){
+			date_default_timezone_set("America/Thule");
+			$gmtime = gmdate("y/m/d h:i:s", time() + $gmt*3600);
+			return $gmtime;
+			//
+		}
 
-	function get_time_zone($gmt){
-		date_default_timezone_set("America/Thule");
-		$gmtime = gmdate("y/m/d h:i:s", time() + $gmt*3600);
-		return $gmtime;
-		//
+		function upload_image(){
+			$upload_dir= "uploads/".$_GET['controller']."/";
+			$file_dir = $upload_dir.basename($_FILES['avatar']['name']);
+			$imgfileType = pathinfo($file_dir, PATHINFO_EXTENSION);
+			$_FILES['avatar']['name'] = $_POST['username'].".".$imgfileType;
+			$file_dir = $upload_dir.basename($_FILES['avatar']['name']);
+			move_uploaded_file($_FILES['avatar']['tmp_name'], $file_dir);
+		}
 	}
-
-	function upload_image(){
-		$upload_dir= "uploads/".$_GET['controller']."/";
-		$file_dir = $upload_dir.basename($_FILES['avatar']['name']);
-		$imgfileType = pathinfo($file_dir, PATHINFO_EXTENSION);
-		$_FILES['avatar']['name'] = $_POST['username'].".".$imgfileType;
-		$file_dir = $upload_dir.basename($_FILES['avatar']['name']);
-		move_uploaded_file($_FILES['avatar']['tmp_name'], $file_dir);
-	}
-
-	
-}
-
-
 ?>
